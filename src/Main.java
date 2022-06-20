af34bfa008f68aa80fe37a72a818b593989a330b
Julia Kostomarova <kostomarova.julia@gmail.com>
2022-06-20 12:06:40 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 40000;
+        int amountRuble = 20;
+        int bonusMile = ticketPrice / amountRuble;
+
+        System.out.println("Вам начислено " + bonusMile + " бонусных милей");
+
+    }
+}
